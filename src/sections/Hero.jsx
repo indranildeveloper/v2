3cc4e8c26911a69907b8ac8c8d201599c8185ba6
Indranil Halder <indranilhalder.dev@gmail.com>
2022-09-25 02:06:45 +0530

    Typing effect

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,12 +1,24 @@
+import Typed from "react-typed";
+
 const Hero = () => {
   return (
     <section id="hero" className="flex items-center my-32">
       <div className="container w-full md:w-3/5 lg:w-3/4 mx-auto flex flex-col items-start gap-10 px-10">
         <p className="font-mono text-primary">Hi, My name is</p>
         <h1 className="text-6xl md:text-8xl font-bold">Indranil Halder</h1>
-        <h4 className="text-2xl md:text-4xl font-bold">
-          I build Web Applications
-        </h4>
+
+        <Typed
+          strings={[
+            "I build Single Page Applications",
+            "I build Full Stack Applications",
+            "I build Awesome Experiences",
+          ]}
+          typeSpeed={60}
+          backSpeed={60}
+          loop
+          className="text-2xl md:text-4xl font-bold text-primary"
+        />
+
         <p className="text-lg md:text-xl max-w-4xl">
           I am a full stack(MERN) software engineer specializing in building
           (and occasionally designing) exceptional web experiences. Currently, I
